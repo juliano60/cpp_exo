987629ae2204890ec145cbf6a12239231b659b30
Jules Konan <juliano60@hotmail.co.uk>
2018-11-01 15:11:11 +0000

    Displaying instructions on startup

--- ch6/Driver.cpp
+++ ch6/Driver.cpp
@@ -9,12 +9,15 @@
 // add support for more operators
 // add GUI
 
+void displayInstructions();
+
 int main() {
 	calculator::TokenStream* ts{new calculator::TokenStreamImpl};
 	calculator::Parser ps;
 	ps.setTokenStream(ts);
 
 	try {
+		displayInstructions();
 		std::cout << "> ";
 
 		while (std::cin) {
@@ -43,3 +46,11 @@ int main() {
 
 	return 0;
 }	
+
+void displayInstructions() {
+	std::cout << "Welcome to our Calculator App!\n"
+			<< "Enter expressions followed by a print character (';' or Enter).\n"
+			<< "Operators currently supported are +,-,*,/.\n"
+			<< "Multiple expressions can be entered on the same line\n"
+			<< "by separating them by a ';'.\n";
+}
